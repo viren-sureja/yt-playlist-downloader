b3026ca861f742ef1c5543baeda15fe595303b35
viren sureja <virensureja3@gmail.com>
2023-06-14 11:55:03 +0530

    Updated to show size in Mega bytes

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 from pytube import Playlist
 from pytube import YouTube
 from tqdm import tqdm
+import math
+import time
 
 # Hardcoded playlist URL
 playlist_url = "https://www.youtube.com/playlist?list=PLFr_jkwUp0hjcRpskdwHubPXLXeSKcZ5r"
@@ -23,12 +25,15 @@ for video_url in video_urls:
     # Create a YouTube object for the current video
     video = YouTube(video_url)
 
-    # Retrieve the video file size
+    # Retrieve the video file size in bytes
     response = requests.head(video_url)
-    file_size = int(response.headers.get('Content-Length', 0))
+    file_size_bytes = int(response.headers.get('Content-Length', 0))
+
+    # Calculate the video file size in megabytes (MB)
+    file_size_mb = file_size_bytes / (1024 * 1024)
 
     # Initialize inner progress bar for current video
-    inner_pbar = tqdm(total=file_size, desc=f"Downloading {video.title}", unit='B', unit_divisor=1024, leave=False)
+    inner_pbar = tqdm(total=file_size_mb, desc=f"Downloading {video.title}", unit='MB', unit_divisor=1024, leave=False)
 
     retries = 0
     success = False
@@ -40,9 +45,15 @@ for video_url in video_urls:
             chunk_size = 1024 * 1024  # 1 MB
             response = requests.get(stream.url, stream=True)
             with open(f"{video.title}.{stream.subtype}", "wb") as file:
+                bytes_downloaded = 0
+                start_time = time.time()
                 for chunk in response.iter_content(chunk_size=chunk_size):
                     file.write(chunk)
-                    inner_pbar.update(len(chunk))
+                    bytes_downloaded += len(chunk)
+                    progress_fraction = min(bytes_downloaded / file_size_bytes, 1.0)
+                    inner_pbar.update(progress_fraction * file_size_mb)
+                    download_speed = bytes_downloaded / (time.time() - start_time)
+                    inner_pbar.set_postfix(speed=f"{download_speed:.2f} MB/s")
 
             success = True
         except Exception as e:
